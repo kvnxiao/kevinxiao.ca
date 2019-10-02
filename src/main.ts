4cb2104be042d1eeb8835ac908d589655eec6249
Kevin Xiao <xlzqwerty1@gmail.com>
2019-10-02 10:25:52 -0700

    Remove vuex

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import Vue from "vue"
 import App from "./App.vue"
 import router from "./router"
-import store from "./store"
 import { library } from "@fortawesome/fontawesome-svg-core"
 import {
   faGithubAlt,
@@ -19,6 +18,5 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
   render: h => h(App),
 }).$mount("#app")
