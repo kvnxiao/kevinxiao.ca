037b7e656f90781fa42ee32ab6c377f9c192afe8
Kevin Xiao <xlzqwerty1@gmail.com>
2019-10-02 16:34:43 -0700

    Append rel=noopener to target=_blank links

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,16 +9,16 @@
       | .
     button.button.is-light Explore my projects
     .social
-      a.button.is-medium.is-white(href="https://www.linkedin.com/in/kvnxiao/" target="_blank" aria-label="LinkedIn profile")
+      a.button.is-medium.is-white(href="https://www.linkedin.com/in/kvnxiao/" aria-label="LinkedIn profile" target="_blank" rel="noopener")
         span.icon
           fa(:icon="['fab', 'linkedin-in']")
-      a.button.is-medium.is-white(href="https://github.com/kvnxiao/" target="_blank" aria-label="GitHub profile")
+      a.button.is-medium.is-white(href="https://github.com/kvnxiao/" aria-label="GitHub profile" target="_blank" rel="noopener")
         span.icon
           fa(:icon="['fab', 'github-alt']")
       button.button.is-medium.is-white.tooltip.is-tooltip-bottom(@click="copyDiscord" data-tooltip="alpha;helix#1222 (copy to clipboard)" aria-label="Discord profile")
         span.icon
           fa(:icon="['fab', 'discord']")
-      a.button.is-medium.is-white(href="https://steamcommunity.com/id/kvnxiao/" target="_blank" aria-label="Steam profile")
+      a.button.is-medium.is-white(href="https://steamcommunity.com/id/kvnxiao/" aria-label="Steam profile" target="_blank" rel="noopener")
         span.icon
           fa(:icon="['fab', 'steam-symbol']")
 </template>
